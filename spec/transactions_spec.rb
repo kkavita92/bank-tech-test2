69760202a5641de9661c5fb3cc4e49335c8d36c3
Kavita <kkavita92@gmail.com>
2017-07-11 13:41:20 +0100

    Refactor out Date.new into variable within transactions_spec

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -2,19 +2,17 @@ require 'transactions'
 
 describe Transactions do
   subject(:transactions) { described_class.new }
+  let(:date_today) { Date.new }
 
   describe '#add_credit_transaction' do
     it 'adds new credit transaction to list' do
-      expect(transactions.add_credit_transaction(100)).to eq [{amount: 100, date: Date.new}]
+      expect(transactions.add_credit_transaction(100)).to eq [{amount: 100, date: date_today}]
     end
   end
 
   describe '#add_debit_transaction' do
     it 'adds new debit transaction to list' do
-      expect(transactions.add_debit_transaction(100)).to eq [{amount: -100, date: Date.new}]
+      expect(transactions.add_debit_transaction(100)).to eq [{amount: -100, date: date_today}]
     end
   end
-
-
-
 end
