df69eaccd7b2c4723a4006e0cfba4acf36ae629c
Kavita <kkavita92@gmail.com>
2017-07-11 13:39:43 +0100

    Refactor second test in transactions_spec

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -4,8 +4,14 @@ describe Transactions do
   subject(:transactions) { described_class.new }
 
   describe '#add_credit_transaction' do
-    it 'adds new transaction to list' do
+    it 'adds new credit transaction to list' do
       expect(transactions.add_credit_transaction(100)).to eq [{amount: 100, date: Date.new}]
+    end
+  end
+
+  describe '#add_debit_transaction' do
+    it 'adds new debit transaction to list' do
+      expect(transactions.add_debit_transaction(100)).to eq [{amount: -100, date: Date.new}]
     end
   end
 
